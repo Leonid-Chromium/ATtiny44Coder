6dd7f634745f42a3237f3808aaea686ff6cf92dc
Leonid-Chromium <l.khromenko@gmail.com>
2021-09-24 16:16:15 +0300

    end

---
+++ ATtyni44Coder/ATtyni44Coder/main.c
@@ -0,0 +1,20 @@
+/*
+ * ATtyni44Coder.c
+ *
+ * Created: 20.05.2021 8:44:42
+ * Author : Leo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		//мне нужно кодер
+		
+    }
+}
+
